f63ebfc3d4f2e03cbb7bc3872c93bafef43685ad
SVLoginova <lsvetvik@mail.ru>
2023-10-18 22:42:55 +0300

    задача 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+
+    return number;
+}
+int numberA = ReadInt("Введите число A");
+int numberB = ReadInt("Введите степень числа");
+int stepen = 1;
+for (int i =1; i <= numberB; i++ )
+    {
+    stepen = stepen * numberA;
+    }
+Console.WriteLine(stepen);
