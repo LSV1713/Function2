d8e03ce7c8ccf9c2721abac3784f6912079266fd
SVLoginova <lsvetvik@mail.ru>
2023-10-18 22:52:05 +0300

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+
+    return number;
+}
+int number = ReadInt("Введите число");
+int summa = 0;
+while (number > 0)
+    {
+    summa = summa + number% 10;
+    number = number /10;
+    }
+Console.WriteLine(summa);
